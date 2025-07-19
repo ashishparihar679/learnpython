ec5395ed0eec96946314b90a9ed85b91ff17bf1d
ashishparihar679 <ashishparihar679@gmail.com.>
2025-07-19 23:50:43 +0530

    30 now

--- python/03pettern/p1.py
+++ python/03pettern/p1.py
@@ -89,4 +89,11 @@ for i13 in range(5,0,-1):
     for l13 in range(5,i5-1,-1):
         print("7",end=" ")
     print()
+for i14 in range(5,0,-1):
+    for j14 in range(1,i5):
+        print(" ",end=" ")
+    for l14 in range(5,i5-1,-1):
+        print("7",end=" ")
+    print()
+    
     # wap to take input from user and after the input of second fid all the palindrom number of second input times
