e3d351efe0ac168c6df5923e4e2e53f55c0d33f5
ashishparihar679 <ashishparihar679@gmail.com.>
2026-05-14 23:31:13 +0530

    update

--- basic/task/q10.py
+++ basic/task/q10.py
@@ -4,9 +4,9 @@ if(n==0):
 elif(n<0):
     print("----------------negetive")
 else:
-    print("+++++++++++positive")
+    print("+++++++++**************++positive")
     if(n%2==0):
         print("eeeee888888ven")
 
     else:
-        print("o111111111111111111111111oodd")
+        print("o111110000000000000000001111111111111111111oodd")
