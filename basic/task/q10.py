d628b5ebdc0c6d27cfad8411f7d38f586ec16abe
ashishparihar679 <ashishparihar679@gmail.com.>
2026-05-21 00:04:18 +0530

    aman

--- basic/task/q10.py
+++ basic/task/q10.py
@@ -2,11 +2,11 @@ n = int (input("enter number"))
 if(n==0):
     print("zero")
 elif(n<0):
-    print("negetiveeeeee")
+    print("idkckfk")
 else:
     print("+++++++++******-")
     if(n%2==0):
-        print("hfjffkfkfk")
+        print("ijnfkfen")
 
     else:
         print("odd")
