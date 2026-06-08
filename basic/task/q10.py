9cd950121b42a3263134af81c86ea7cd13ea8b80
ashishparihar679 <ashishparihar679@gmail.com.>
2026-06-08 23:01:09 +0530

    hiiiiiiiii

--- basic/task/q10.py
+++ basic/task/q10.py
@@ -2,7 +2,7 @@ n = int (input("enter number please"))
 if(n==0):
     print("zero")
 elif(n<0):
-    print("last exam on 8th jun")
+    print("last exam on 8th jun 11 baje")
 else:
     print("-------------")
     if(n%2==0):
