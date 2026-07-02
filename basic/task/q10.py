d225d6933b695178064ab6c5a13c7ad0c7ab406b
ashishparihar679 <ashishparihar679@gmail.com.>
2026-07-02 10:31:18 +0530

    jejjd

--- basic/task/q10.py
+++ basic/task/q10.py
@@ -2,11 +2,11 @@ n = int (input("enter number please"))
 if(n==0):
     print("zero")
 elif(n<0):
-    print("less than zero")
+    print("kjdjdjdj than zero")
 else:
     print("-------------")
     if(n%2==0):
-        print("last")
+        print("end")
 
     else:
         print("i will go to banglore")
