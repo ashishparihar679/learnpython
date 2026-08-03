f88b95b5f2a00b0b9bfcb18cdb165be6cc86d026
ashishparihar679 <ashishparihar679@gmail.com.>
2026-08-03 20:48:05 +0530

    hhh

--- basic/6_loop/for11q9.py
+++ basic/6_loop/for11q9.py
@@ -3,4 +3,4 @@ for i in number:
     if number % number == 0 and number % 1:
      print("number is prime")
     else:
-     print("number is not prime number")
+     print("one week complete to leave home")
