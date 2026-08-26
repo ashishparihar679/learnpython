c20717d50032451a7deb5ad90b56e01738f4bb71
ashishparihar679 <ashishparihar679@gmail.com.>
2026-08-27 00:04:33 +0530

    changes

---
+++ basic2/01operatiors/p35.py
@@ -0,0 +1,13 @@
+l=0
+while(True):
+    n=input("entre pass : ")
+    l=l+1
+    if n=='a@123':
+        print("CORRECT PASSWORD")
+        break
+    elif l==3:
+        print("PASSWORD LIMIT REACHED")
+        break
+    else:
+        print("INCORRECT PASSWORD PLEASE ENTER CORRECT PASSWORD")
+    
