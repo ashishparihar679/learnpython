1d7c56f361585d250730d75be4fa2a000919c78c
ashishparihar679 <ashishparihar679@gmail.com.>
2026-08-29 14:27:09 +0530

    hii

--- basic2/01operatiors/p37.py
+++ basic2/01operatiors/p37.py
@@ -1,6 +1,6 @@
-t1=(10,1.4,4+6j,True,'don')
-# t1[0]=1000 TypeError: 'tuple' object does not support item assignment
-t1=(10,20,30)
-l1=[10,20,30]
-print(t1.__sizeof__())
-print(l1.__sizeof__())
+# l1=[10,20,30,40,50]
+# l2=l1[::-1]
+# print(l2)
+l1=(10,20,30,40,50)
+l2=l1[len(l1)::-1]
+print(l2)
