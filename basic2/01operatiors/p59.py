9866c384c408e9f95fa7ac1d1da6dbe8613061c9
ashishparihar679 <ashishparihar679@gmail.com.>
2026-09-12 00:22:03 +0530

    12SEP2026

---
+++ basic2/01operatiors/p59.py
@@ -0,0 +1,22 @@
+#COUNT()
+
+s = 'Iron man and spiderman'
+# print(s.count('a'))
+# print(s.count('a',9))
+# print(s.count('a',9,17))
+# print(s.count('x'))
+# print(s.count('I'))
+# print(s.count('I',4))
+
+# print(s.count('man'))
+# print(s.count('mAn'))
+# print(len(s))
+# print(s.count('oo'))#non over laping
+# print(s.count(''))
+
+# INDEX()
+s1='pyspiders'
+# print(s1.index('s'))
+# print(s1.rindex('s'))
+print(s1.index('d'))
+print(s1.rindex('d'))
