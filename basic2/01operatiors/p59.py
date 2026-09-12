40a993c0548c5441d246be12ce467eeeb8988b08
ashishparihar679 <ashishparihar679@gmail.com.>
2026-09-13 00:07:03 +0530

    13SEP2026

--- basic2/01operatiors/p59.py
+++ basic2/01operatiors/p59.py
@@ -15,8 +15,31 @@ s = 'Iron man and spiderman'
 # print(s.count(''))
 
 # INDEX()
-s1='pyspiders'
-# print(s1.index('s'))
-# print(s1.rindex('s'))
-print(s1.index('d'))
-print(s1.rindex('d'))
+# RINDEX()
+s1 = "pyspiders"
+
+print(s1.index('s'))      # 2
+print(s1.rindex('s'))     # 8
+
+print(s1.index('d'))      # 5
+print(s1.rindex('d'))     # 5
+
+print(s1.index('sp'))     # 2
+print(s1.rindex('sp'))    # 2
+
+# print(s1.rindex('sP'))    # ValueError: substring not found
+# print(s1.index('l'))      # ValueError: substring not found
+# print(s1.rindex('l'))     # ValueError: substring not found
+#FIND
+#RFIND
+
+s2 = "pyspiders"
+
+print(s2.find('s'))      # 2
+print(s2.rfind('s'))     # 8
+print(s2.find('l'))      # -1
+# print(s2.find('sP'))     # -1
+
+s3 = "Iron man and spiderman"
+print(s3.find('man'))    # 5
+print(s3.find('mAn'))    # -1
