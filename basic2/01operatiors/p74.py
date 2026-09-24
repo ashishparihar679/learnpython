9e797eb4078b6fb3f519bb129d83f17917dafe72
ashishparihar679 <ashishparihar679@gmail.com.>
2026-09-25 00:11:53 +0530

    25SEP2026

--- basic2/01operatiors/p74.py
+++ basic2/01operatiors/p74.py
@@ -2,6 +2,8 @@ s="hello sam" #HeLL@Sa
 print(s)
 
 d = {'h':'H','l':'L','o':'@','m':''}
+# d = {'h':'H','l':'L','o':'@','m':''}
+# d = {'h':'H','l':'L','o':'@','m':''}
 
 table=str.maketrans(d)
 print(table)
